4edf328eb9aeda550334e5acde55beb11b7dc992
Julien Etienne <j8@envidia.io>
2022-05-10 22:34:59 +0100

    Added counter example

--- examples/animated-text/script.js
+++ examples/animated-text/script.js
@@ -1,22 +1,23 @@
 import { insertInto } from '../../anti-framework.js'
-const raf = window.requestAnimationFrame;
+const raf = window.requestAnimationFrame
 const greeting = 'Welcome to Anti-Framework'
 const text = greeting.split('')
 let fwd = true
 let i = 1
 let j = 0
+
 const helloView = insertInto('#root', ({ greeting }) => `<h1>${greeting}</h1>`)
 helloView({ greeting })
 
 const loop = (timestamp) => {
-    j++
-    if(j === 5 ) {
-        if (i === 1) fwd = true
-        if (i === text.length) fwd = false
-        fwd ? i++ : i--
-        helloView({ greeting: text.slice(0,i).join('')})
-        j = 0
-    }
-    raf(loop)    
+  j++
+  if (j === 5) {
+    if (i === 1) fwd = true
+    if (i === text.length) fwd = false
+    fwd ? i++ : i--
+    helloView({ greeting: text.slice(0, i).join('') })
+    j = 0
+  }
+  raf(loop)
 }
 raf(loop)

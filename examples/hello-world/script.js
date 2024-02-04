0da3832c4723b70fb8dc4013e54bf1d324ec15c5
Julien Etienne <j8@envidia.io>
2024-02-04 07:55:52 +0000

    Updated reserve branch

--- examples/hello-world/script.js
+++
@@ -1,4 +0,0 @@
-import { insertInto } from '../../anti-framework.js'
-
-const helloView = insertInto('#root', ({ greeting }) => `<h1>${greeting}</h1>`)
-helloView({ greeting: 'Hello World!' })
--- sandbox/full-api/controller.js
+++
@@ -1,9 +0,0 @@
-import { pasteInto, sequence, empty } from '../../src/index.js'
-import { boxView, frameView } from './views.js'
-
-// Boxes
-const boxesStr = sequence(9, 1).map(boxView).join(empty)
-
-// Paste the Frame View into the root with the boxes
-const frame = pasteInto('/', frameView)
-frame({ boxesStr })
